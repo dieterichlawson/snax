c284e81f37d1c10c3fc22fc5f38a4d2c0ea5c8ae
Dieterich Lawson <dieterich.lawson@gmail.com>
2022-04-20 15:27:29 -0700

    Bumping version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="snax",
-    version="0.0.7",
+    version="0.0.8",
     author="Dieterich Lawson",
     author_email="dieterich.lawson@gmail.com",
     description="A simple deep learning library for JAX.",
