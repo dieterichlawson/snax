6f892397531910acb618f19de52c3828e36436bf
libby <elizabeth.zhang94@gmail.com>
2022-12-12 12:13:07 -0800

    optionally save treedef with data; auto-load data with treedef

--- snax/checkpoint.py
+++ snax/checkpoint.py
@@ -7,6 +7,27 @@ PyTreeDef = Any
 
 Data = TypeVar('Data')
 
+def get_checkpoints(
+        checkpoint_dir: str,
+        name_prefix: str = "checkpoint",
+        filetype: str = ".chk") -> List[Path]:
+  """Get all paths to checkpoints in directory."""
+  checkpoint_glob = name_prefix + '*' + filetype
+  checkpoint_paths = Path(checkpoint_dir).glob(checkpoint_glob)
+  return list(checkpoint_paths)
+
+def step_from_path(path: Path) -> int:
+  return int(path.stem.split("_")[1])
+
+def save_checkpoint_to_path(
+        data: Any,
+        step: int,
+        path: Path,
+        treedef: Optional[PyTreeDef]=None) -> None:
+  path.parent.mkdir(parents=True, exist_ok=True)
+  with path.open(mode='wb') as f:
+    pickle.dump((data, step, treedef), f)
+
 def save_checkpoint(
         data: Any,
         step: int,
@@ -14,7 +35,10 @@ def save_checkpoint(
         num_checkpoints_to_keep: int = 3,
         name_prefix: str = "checkpoint",
         step_format: str = "{step:08d}",
-        filetype: str = ".chk") -> None:
+        filetype: str = ".chk",
+        treedef: Optional[PyTreeDef]=None) -> None:
+
+  # Get exisiting checkpoints in directory
   existing_checkpoints = get_checkpoints(
           checkpoint_dir,
           name_prefix=name_prefix,
@@ -23,23 +47,16 @@ def save_checkpoint(
   assert len(checkpoint_steps) <= num_checkpoints_to_keep, "Too many checkpoints saved."
   assert len(checkpoint_steps) == len(set(checkpoint_steps)), "Non-unique checkpoint step."
   assert all([step > s for s in checkpoint_steps]), "Attempting to write earlier checkpoint."
+
+  # Remove oldest checkpoint, if max num_checkpoints_to_keep reached
   if len(checkpoint_steps) == num_checkpoints_to_keep:
     to_remove = checkpoint_steps.index(min(checkpoint_steps))
     existing_checkpoints[to_remove].unlink()
+
+  # Save checkpoint
   new_name = name_prefix + "_" + step_format.format(step=step) + filetype
   new_path = Path(checkpoint_dir) / new_name
-  save_checkpoint_to_path(data, step, new_path)
-
-def get_checkpoints(
-        checkpoint_dir: str,
-        name_prefix: str = "checkpoint",
-        filetype: str = ".chk") -> List[Path]:
-  checkpoint_glob = name_prefix + '*' + filetype
-  checkpoint_paths = Path(checkpoint_dir).glob(checkpoint_glob)
-  return list(checkpoint_paths)
-
-def step_from_path(path: Path) -> int:
-  return int(path.stem.split("_")[1])
+  save_checkpoint_to_path(data, step, new_path, treedef)
 
 def get_latest_checkpoint_path(
         checkpoint_dir: str,
@@ -52,6 +69,19 @@ def get_latest_checkpoint_path(
   max_ind = steps.index(max(steps))
   return paths[max_ind]
 
+def load_checkpoint_from_path(path: Path) -> Tuple[Any, int]:
+  # Load checkpoint
+  with path.open(mode='rb') as f:
+    data, step, treedef = pickle.load(f)
+  
+  # If PyTreeDef saved, unflatten model accordingly
+  if treedef is not None:
+    loaded_leaves, _ = jax.tree_util.tree_flatten(data)
+    loaded_leaves = [jax.numpy.array(x) for x in loaded_leaves]
+    data = treedef.unflatten(loaded_leaves)
+
+  return data, step
+
 def load_latest_checkpoint(
         checkpoint_dir: str,
         name_prefix: str = "checkpoint",
@@ -61,25 +91,16 @@ def load_latest_checkpoint(
     return None, None
   return load_checkpoint_from_path(path)
 
-def load_checkpoint_from_path(path: Path) -> Tuple[Any, int]:
-  with path.open(mode='rb') as f:
-    data, step = pickle.load(f)
-  return data, step
-
-def save_checkpoint_to_path(data: Any, step: int, path: Path) -> None:
-  path.parent.mkdir(parents=True, exist_ok=True)
-  with path.open(mode='wb') as f:
-    pickle.dump((data, step), f)
-
 def load_latest_checkpoint_with_treedef(
         treedef: PyTreeDef,
         checkpoint_dir: str,
         name_prefix: str = "checkpoint",
         filetype: str = ".chk") -> Union[Tuple[None, None], Tuple[Any, int]]:
+  """Load latest checkpoint file, with explicit treedef passed in."""
   path = get_latest_checkpoint_path(checkpoint_dir, name_prefix=name_prefix, filetype=filetype)
   if path is None:
     return None, None
-  new_model, step = load_checkpoint_from_path(path)
+  new_model, step, _ = load_checkpoint_from_path(path)
   loaded_leaves, _ = jax.tree_util.tree_flatten(new_model)
   loaded_leaves = [jax.numpy.array(x) for x in loaded_leaves]
   restored_model = treedef.unflatten(loaded_leaves)
